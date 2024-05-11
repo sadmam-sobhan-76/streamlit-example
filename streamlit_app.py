e1e98b279e9ccdd3a6f289e50c90118365f30979
sadmam-sobhan-76 <114879887+sadmam-sobhan-76@users.noreply.github.com>
2024-05-11 22:23:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+import streamlit as st
+from PIL import Image
+
 # Set page config
 st.set_page_config(
     page_title="HR Recruitment Expert System",
@@ -11,17 +14,26 @@ st.set_page_config(
 )
 
 # Set RIA Money Transfer color scheme
+primary_color = "#F15A24"
+secondary_color = "#0074C8"
+background_color = "#FFFFFF"
+
 st.markdown(
-    """
+    f"""
     <style>
-    body {
+    .reportview-container {{
+        background-color: {background_color};
+    }}
+    .sidebar .sidebar-content {{
+        background-color: {primary_color};
         color: white;
-        background-color: #F15A24;
-    }
-    .stButton button {
-        background-color: #0074C8;
-        border-color: #0074C8;
-    }
+    }}
+    .Widget .content {{
+        color: {primary_color};
+    }}
+    .stButton button {{
+        background-color: {secondary_color};
+    }}
     </style>
     """,
     unsafe_allow_html=True
@@ -29,7 +41,10 @@ st.markdown(
 
 # Header
 st.title("HR Recruitment Expert System")
-st.write("Welcome to RIA Money Exchange's HR Recruitment Expert System.")
+
+# Load company logo
+logo = Image.open(""C:\Users\Sadmam Sobhan\Desktop\Academic Project\Ria_Main_Logo_Descriptor_Color_RGB.png"")
+st.image(logo, use_column_width=True)
 
 # Main content
 st.header("Vetting the Hiring Process")
